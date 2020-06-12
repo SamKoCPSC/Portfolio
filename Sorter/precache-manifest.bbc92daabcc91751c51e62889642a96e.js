e8c146137ae0ca558317672272144c9e3c05ba0a
SamuelKo <m2z2b@ugrad.cs.ubc.ca>
2020-06-12 04:08:16 -0700

    Moving files

---
+++ Sorter/precache-manifest.bbc92daabcc91751c51e62889642a96e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ffc15ac83c6cb7df61d9dbae13c570",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abfbe11f82227b418462",
+    "url": "./static/css/main.6a1a2758.chunk.css"
+  },
+  {
+    "revision": "b4de3fd2eaafd8b2e081",
+    "url": "./static/js/2.e9fc7598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e9fc7598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abfbe11f82227b418462",
+    "url": "./static/js/main.9e38e72a.chunk.js"
+  },
+  {
+    "revision": "3f42224c6d809939f50e",
+    "url": "./static/js/runtime-main.8a0672bf.js"
+  }
+]);
